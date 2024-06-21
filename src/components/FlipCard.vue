ba81e51e7139bb3a67585cd0066da9b9850d26a9
Filbert Crasto <filbertcrasto619@gmail.com>
2024-06-22 00:11:36 +0100

    Fix Colour

--- src/components/FlipCard.vue
+++ src/components/FlipCard.vue
@@ -9,10 +9,10 @@
   <div class="flip-card rounded" :class="{'reveal' : revealScore}">
     <div class="flip-card-inner">
       <div class="flip-card-front">
-        <div class="bg-gradient-to-tr from-pink-600 to-pink-400 h-full w-full"></div>
+        <div class="h-full w-full"  :class="user.score === 0 ? 'bg-gray-300' : 'bg-pattern'"  ></div>
       </div>
-      <div class="flip-card-back bg-sky-300 border-2 border-indigo-500/100">
-        <h1>{{user.score}}</h1>
+      <div class="flip-card-back  border-2 border-sky-500">
+        <h1 class="font-extrabold text-lg" >{{user.score}}</h1>
       </div>
     </div>
   </div>
@@ -26,12 +26,11 @@
 
 .flip-card-inner {
   position: relative;
-  width: 40px;
-  height: 50px;
+  width: 50px;
+  height: 80px;
   text-align: center;
   transition: transform 0.6s;
   transform-style: preserve-3d;
-  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 }
 
 .flip-card.reveal .flip-card-inner {
@@ -44,6 +43,11 @@
   height: 100%;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
+}
+
+.bg-pattern {
+  background-color: #000000;
+  background-image: url("data:image/svg+xml,%3Csvg width='24' height='20' viewBox='0 0 24 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20 18c0-1.105.887-2 1.998-2 1.104 0 2-.895 2.002-1.994V14v6h-4v-2zM0 13.998C0 12.895.888 12 2 12c1.105 0 2 .888 2 2 0 1.105.888 2 2 2 1.105 0 2 .888 2 2v2H0v-6.002zm16 4.004A1.994 1.994 0 0 1 14 20c-1.105 0-2-.887-2-1.998v-4.004A1.994 1.994 0 0 0 10 12c-1.105 0-2-.888-2-2 0-1.105-.888-2-2-2-1.105 0-2-.887-2-1.998V1.998A1.994 1.994 0 0 0 2 0a2 2 0 0 0-2 2V0h8v2c0 1.105.888 2 2 2 1.105 0 2 .888 2 2 0 1.105.888 2 2 2 1.105 0 2-.888 2-2 0-1.105.888-2 2-2 1.105 0 2-.888 2-2V0h4v6.002A1.994 1.994 0 0 1 22 8c-1.105 0-2 .888-2 2 0 1.105-.888 2-2 2-1.105 0-2 .887-2 1.998v4.004z' fill='%23ff0056' fill-opacity='0.64' fill-rule='evenodd'/%3E%3C/svg%3E");
 }
 
 .flip-card-back {
